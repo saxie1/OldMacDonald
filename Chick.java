6ccd69a612c0bacbc04974ab7f8b338c42c6db3e
Sam X <samx@Sams-MacBook-Pro.local>
2018-10-26 01:58:01 -0700

    Finished

--- Chick.java
+++ Chick.java
@@ -1,4 +1,19 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound , String soundtwo)     {         
+         myType = type;         
+         double x = Math.random();
+         if(x<0.5){
+           mySound = sound;     
+         }else{
+           mySound = soundtwo;
+         }
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,7 +1,7 @@
-
-void setup()
-{
-	//your variable declarations and
-	//System.out.println() statements here
+public void setup() {     
+    Cow c = new Cow("cow", "moo");
+    Chick ch = new Chick("chick","cheep", "cluck");
+    Pig p = new Pig("pig", "oink");
+    Farm macs = new Farm();
+    macs.animalSounds();
 }
-
